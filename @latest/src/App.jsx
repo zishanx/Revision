e388d2871a960dbaa51d55a56b6224032028bfe8
zishanx <zishanalikhan53@gmail.com>
2026-08-07 21:31:40 +0530

    The basic react bug

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      
-    </>
-  )
-}
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(count);
+      setCount(count + 1)
+    }, 1000)
 
-export default App
+    return () => clearInterval(timer)
+  },[])
+
+  return <h1>{count}</h1>
+}
