ecf57b829c84e4db817cd73c1b2335905b7ddab5
zishanx <zishanalikhan53@gmail.com>
2026-08-14 21:56:01 +0530

    REvising what we have learned till now.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,2 @@
-function Dog(name) {
-    this.name = name
-}
-
-Dog.prototype.bark = function () {
-    console.log(this.name + " says Woof!")
-}
-
-const d1 = new Dog()
-
-d1.bark()
-
-
-
+Closure and this . 
+ 
