7ea650b26f5927c6d4fe94990dc3c109607c1ab7
zishanx <zishanalikhan53@gmail.com>
2026-08-14 22:06:21 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,16 @@
-Closure and this . 
+// Closure and this . c
+
+const person = {
+    name: "Zizzy",
+    greet: () => { console.log(this.name);}
+}
  
+
+const timer = {
+    seconds = 10 ,
+    start: function() {
+        setTimeout(() => {
+            console.log(this.seconds)
+        },1000)
+    }
+}
